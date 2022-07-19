838413bd03ab49040dd808181a977e0b5f065863
Nik-W <niko00046@ya.ru>
2022-07-19 17:34:04 +0300

    Redesigned in Creatio style

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,18 +1,18 @@
 export const products = [
   {
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'Basic',
+    price: 499,
+    description: 'Minimum required functionality'
   },
   {
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'Standard',
+    price: 999,
+    description: 'Classic version'
   },
   {
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'Extended',
+    price: 1999,
+    description: 'Feel like a god'
   }
 ];
 
